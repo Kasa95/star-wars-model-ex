122e4a5ec053b5f729bd2b7932cdc40182632c04
Kasa95 <nachocasanova95@gmail.com>
2022-06-17 18:41:35 +0000

    v2 diagrama

--- src/models.py
+++ src/models.py
@@ -13,6 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(32), nullable=False)
     password = Column(String(64), nullable=False)
+    favorites = relationship('Favorite', backref='user', lazy=True)
 
 class Planet(Base):
     __tablename__ = 'planets'
@@ -20,6 +21,8 @@ class Planet(Base):
     name = Column(String(64), nullable=False)
     climate = Column(String(32), nullable=True)
     population = Column(Integer, nullable=True)
+    favorite_user = relationship('Favorite', backref='planets', lazy=True)
+    favorite_character = relationship('Character', backref='planets', lazy=True)
 
 class Vehicle(Base):
     __tablename__ = 'vehicles'
@@ -27,6 +30,8 @@ class Vehicle(Base):
     name = Column(String(64), nullable=False)
     manufacturer = Column(String(64), nullable=True)
     max_speed = Column(Integer, nullable=True)
+    favorite_user = relationship('Favorite', backref='vehicles', lazy=True)
+    favorite_character = relationship('Character', backref='vehicles', lazy=True)
 
 class Character(Base):
     __tablename__ = 'characters'
@@ -36,9 +41,8 @@ class Character(Base):
     hair = Column(String(32), nullable=True)
     planet_id = Column(Integer, ForeignKey('planets.id'))
     vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    favorite_user = relationship('Favorite', backref='characters', lazy=True)
 
-    planet = relationship(Planet)
-    vehicle = relationship(Vehicle)
 
 class Favorite(Base):
     __tablename__ = 'favorites'
@@ -47,11 +51,7 @@ class Favorite(Base):
     planet_id = Column(Integer, ForeignKey('planets.id'))
     character_id = Column(Integer, ForeignKey('characters.id'))
     vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
-    
-    user = relationship(User)
-    planet = relationship(Planet)
-    character = relationship(Character)
-    vehicle = relationship(Vehicle)
+
 
     def to_dict(self):
         return {}
